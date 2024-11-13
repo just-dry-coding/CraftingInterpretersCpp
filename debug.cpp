215edb34f88da05f79cd9a6afea272650ebf6f84
Justin Grünenwald <jug@cado.camos.de>
2024-11-13 12:02:55 +0100

    refactor debug simplifying functions

--- debug.cpp
+++ debug.cpp
@@ -10,25 +10,18 @@ int simpleInstruction(std::string const&, int);
 void disassembleChunk(Chunk const& chunk, std::string const& name)
 {
 	std::print("== {} ==\n", name);
-	for (unsigned int offset = 0; offset < chunk.size();) {
-		offset = disassembleInstruction(chunk[offset], offset);
+	for (unsigned int offset = 0; offset < chunk.size(); ++offset) {
+		std::print("{}\t", offset);
+		disassembleInstruction(chunk[offset], offset);
 	}
 }
 
-int disassembleInstruction(Instruction const& instruction, int offset)
+void disassembleInstruction(Instruction const& instruction)
 {
-	std::print("{}\t", offset);
 	switch (instruction.opCode) {
 	case OpCode::OP_RETURN:
-		return simpleInstruction("OP_RETURN", offset);
+		std::print("OP_RETURN");
 	default:
 		std::print("unknow code");
-		return offset + 1;
 	}
-
-}
-
-int simpleInstruction(std::string const& name, int offset) {
-	std::print("{}\n", name);
-	return offset + 1;
 }
